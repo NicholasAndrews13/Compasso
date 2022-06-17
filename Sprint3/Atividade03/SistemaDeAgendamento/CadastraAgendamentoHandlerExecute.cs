b67abfd379eb079936b43a6da45d5abfabbb6f10
Nicholas Andrews <87037807+NicholasAndrews13@users.noreply.github.com>
2022-06-17 18:25:13 -0300

    Add files via upload

---
+++ Sprint3/Atividade03/SistemaDeAgendamento/CadastraAgendamentoHandlerExecute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace
+{
+
+    public class CadastraAgendamentoHandlerExecute
+    {
+        public CadastraAgendamentoHandlerExecute()
+        {
+
+            public void ExecuteTeste()
+            {
+                var options = new DbContextOptionsBuilder<DbContext>()
+                    .UseInMemoryDatabase("DbContext")
+                    .Options;
+
+                var contexto = new DbContext(options);
+
+                var repo = new RepositorioAgendamento(contexto);
+
+                repo
+
+
+            }
+        }
+    }
+}
