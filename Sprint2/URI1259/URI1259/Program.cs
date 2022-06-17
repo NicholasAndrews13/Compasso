62d258d6186dabda3e19fde36e246fd611136375
Nicholas Andrews <87037807+NicholasAndrews13@users.noreply.github.com>
2022-06-17 18:14:37 -0300

    Add files via upload

---
+++ Sprint2/URI1259/URI1259/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using System.Linq;
+namespace URI1259
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(ReadLine());
+            int[] arr = new int[numero];
+            for (int i = 0; i < arr.Length; i++) arr[i] = int.Parse(ReadLine());
+            foreach (var item in arr.Where(x => x % 2 == 0).OrderBy(x => x)) WriteLine(item);
+            foreach (var item in arr.Where(x => x % 2 != 0).OrderByDescending(x => x)) WriteLine(item);
+        }
+    }
+}
+
+ 
+
+    
+
+ 
