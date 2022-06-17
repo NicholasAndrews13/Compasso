c67d65f7f26d5c4e91aa706e4465f064969a7ab4
Nicholas Andrews <87037807+NicholasAndrews13@users.noreply.github.com>
2022-06-17 18:02:28 -0300

    Add files via upload

---
+++ Sprint2/URI1009/uri1009/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vendedor = Console.ReadLine();
+            double salario = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double vendasValor = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMensal = salario + (vendasValor * 0.15); // salário mais 15 %
+            string salarioMensalFormatado = salarioMensal.ToString("F2", CultureInfo.InvariantCulture);
+            Console.WriteLine($"TOTAL = R$ {salarioMensalFormatado}");
+
+        }
+    }
+}
