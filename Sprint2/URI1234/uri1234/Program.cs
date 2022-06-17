0aaac7557169c9a350648882091a9a71772095f3
Nicholas Andrews <87037807+NicholasAndrews13@users.noreply.github.com>
2022-06-17 18:10:59 -0300

    Add files via upload

---
+++ Sprint2/URI1234/uri1234/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace uri1234
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            StringBuilder saida = new StringBuilder();
+            bool mudar;
+            while ((entrada = Console.ReadLine()) != null)
+            {
+                saida.Clear();
+                mudar = true;
+                foreach (var c in entrada)
+                {
+                    if (!c.Equals(' '))
+                    {
+                        saida.Append(mudar ? char.ToUpper(c) : char.ToLower(c));
+                        mudar = !mudar;
+                    }
+                    else
+                    {
+                        saida.Append(c);
+                    }
+                }
+                Console.WriteLine(saida);
+            }
+        }
+    }
+}
