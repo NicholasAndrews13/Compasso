acf3de6b07df37ea43f1b8cfb771b9782f883588
Nicholas Andrews <87037807+NicholasAndrews13@users.noreply.github.com>
2022-06-17 18:09:55 -0300

    Add files via upload

---
+++ Sprint2/URI1013/uri1013/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ReadKey();
+            string[] numero = Console.ReadLine().Split(' ');
+
+            int maior = RetornaMaior(RetornaMaior(numero[0], numero[1]).ToString(), numero[2]);
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+        public static int RetornaMaior(string numero1, string numero2)
+        {
+            int numero1Int = Int32.Parse(numero1);
+            int numero2Int = Int32.Parse(numero2);
+            return (numero1Int + numero2Int + Math.Abs(numero1Int - numero2Int)) / 2;
+        }
+    }
+}
