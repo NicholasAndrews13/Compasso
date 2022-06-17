e21cd0681a09f44fef314c15a70301f0419520f5
Nicholas Andrews <87037807+NicholasAndrews13@users.noreply.github.com>
2022-06-17 18:16:01 -0300

    Add files via upload

---
+++ Sprint2/URI1507/uri1507/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uri1507
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLinhas = Int32.Parse(Console.ReadLine());
+            List<string> linhas = new List<string>();
+            string conteudo = "";
+            for (int i = 0; i < nLinhas; i++)
+            {
+                conteudo = Console.ReadLine();
+                linhas.Add(conteudo);
+            }
+            if (linhas.Count == 1)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+            if (linhas.Count > 0)
+                for (int i = 0; i < nLinhas; i++)
+                {
+                    if (i + 1 == linhas.Count)
+                    {
+                        break;
+                    }
+
+                    if (linhas[i + 1].Contains(linhas[i]))
+                        Console.WriteLine("Yes");
+                    else
+                        Console.WriteLine("No");
+                }
+            else
+                Console.WriteLine("No");
+
+        }
+    }
+}
